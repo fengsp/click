2850dd69d3d47f74d87825ac0cb9f2170e315fa2
Shipeng Feng <fsp261@gmail.com>
2015-03-03 19:25:29 +0800

    Drop

--- click/core.py
+++ click/core.py
@@ -500,101 +500,15 @@ class BaseCommand(object):
     #: the default for the :attr:`Context.ignore_unknown_options` flag.
     ignore_unknown_options = False
 
-    def make_context(self, info_name, args, parent=None, **extra):
-        """This function when given an info name and arguments will kick
-        off the parsing and create a new :class:`Context`.  It does not
-        invoke the actual command callback though.
-
-        :param info_name: the info name for this invokation.  Generally this
-                          is the most descriptive name for the script or
-                          command.  For the toplevel script it's usually
-                          the name of the script, for commands below it it's
-                          the name of the script.
-        :param args: the arguments to parse as list of strings.
-        :param parent: the parent context if available.
-        :param extra: extra keyword arguments forwarded to the context
-                      constructor.
-        """
-        for key, value in iteritems(self.context_settings):
-            if key not in extra:
-                extra[key] = value
-        ctx = Context(self, info_name=info_name, parent=parent, **extra)
-        self.parse_args(ctx, args)
-        return ctx
-
-    def parse_args(self, ctx, args):
-        """Given a context and a list of arguments this creates the parser
-        and parses the arguments, then modifies the context as necessary.
-        This is automatically invoked by :meth:`make_context`.
-        """
-        raise NotImplementedError('Base commands do not know how to parse '
-                                  'arguments.')
-
-    def invoke(self, ctx):
-        """Given a context, this invokes the command.  The default
-        implementation is raising a not implemented error.
-        """
-        raise NotImplementedError('Base commands are not invokable by default')
-
     def main(self, args=None, prog_name=None, complete_var=None,
              standalone_mode=True, **extra):
         """This is the way to invoke a script with all the bells and
-        whistles as a command line application.  This will always terminate
-        the application after a call.  If this is not wanted, ``SystemExit``
-        needs to be caught.
 
-        This method is also available by directly calling the instance of
-        a :class:`Command`.
-
-        .. versionadded:: 3.0
-           Added the `standalone_mode` flag to control the standalone mode.
-
-        :param args: the arguments that should be used for parsing.  If not
-                     provided, ``sys.argv[1:]`` is used.
-        :param prog_name: the program name that should be used.  By default
-                          the program name is constructed by taking the file
-                          name from ``sys.argv[0]``.
         :param complete_var: the environment variable that controls the
                              bash completion support.  The default is
                              ``"_<prog_name>_COMPLETE"`` with prog name in
                              uppercase.
-        :param standalone_mode: the default behavior is to invoke the script
-                                in standalone mode.  Click will then
-                                handle exceptions and convert them into
-                                error messages and the function will never
-                                return but shut down the interpreter.  If
-                                this is set to `False` they will be
-                                propagated to the caller and the return
-                                value of this function is the return value
-                                of :meth:`invoke`.
-        :param extra: extra keyword arguments are forwarded to the context
-                      constructor.  See :class:`Context` for more information.
         """
-        # If we are in Python 3, we will verify that the environment is
-        # sane at this point of reject further execution to avoid a
-        # broken script.
-        if not PY2:
-            try:
-                import locale
-                fs_enc = codecs.lookup(locale.getpreferredencoding()).name
-            except Exception:
-                fs_enc = 'ascii'
-            if fs_enc == 'ascii':
-                raise RuntimeError('Click will abort further execution '
-                                   'because Python 3 was configured to use '
-                                   'ASCII as encoding for the environment. '
-                                   'Either switch to Python 2 or consult '
-                                   'http://click.pocoo.org/python3/ '
-                                   'for mitigation steps.')
-
-        if args is None:
-            args = sys.argv[1:]
-        else:
-            args = list(args)
-        if prog_name is None:
-            prog_name = make_str(os.path.basename(
-                sys.argv and sys.argv[0] or __file__))
-
         # Hook for the Bash completion.  This only activates if the Bash
         # completion is actually enabled, otherwise this is quite a fast
         # noop.
@@ -620,10 +534,6 @@ class BaseCommand(object):
                 raise
             echo('Aborted!', file=sys.stderr)
             sys.exit(1)
-
-    def __call__(self, *args, **kwargs):
-        """Alias for :meth:`main`."""
-        return self.main(*args, **kwargs)
 
 
 class Command(BaseCommand):
